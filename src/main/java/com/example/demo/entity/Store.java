ced661f76e5fa933717fa6bf1e8f09c6412ccb65
bala-amypo <bala@amypo.in>
2025-12-18 06:45:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/Store.java
+++ src/main/java/com/example/demo/entity/Store.java
@@ -13,6 +13,36 @@ public class Store {
     private String storeName;
 
     private String address;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getStoreName() {
+        return storeName;
+    }
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getRegion() {
+        return region;
+    }
+    public void setRegion(String region) {
+        this.region = region;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
     private String region;
     private Boolean active = true;
 
