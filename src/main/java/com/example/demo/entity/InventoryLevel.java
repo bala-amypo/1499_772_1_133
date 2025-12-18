073b058758b2f45168f9a2280f683b89329f30aa
bala-amypo <bala@amypo.in>
2025-12-18 06:47:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/InventoryLevel.java
+++ src/main/java/com/example/demo/entity/InventoryLevel.java
@@ -26,5 +26,45 @@ public class InventoryLevel {
         lastUpdated = new Timestamp(System.currentTimeMillis());
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Timestamp getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Timestamp lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
     
 }
