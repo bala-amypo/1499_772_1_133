2f38f98a13c70e192a562b6111be69d20605801f
bala-amypo <bala@amypo.in>
2025-12-20 10:33:50 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -8,19 +8,17 @@ import java.util.Map;
 @RequestMapping("/auth")
 public class AuthController {
 
-    // REGISTER
+    
     @PostMapping("/register")
     public String register(@RequestBody Map<String, String> request) {
-        // expected JSON:
-        // { "email": "...", "password": "..." }
+       
         return "User registered successfully";
     }
 
-    // LOGIN
+  
     @PostMapping("/login")
     public String login(@RequestBody Map<String, String> request) {
-        // expected JSON:
-        // { "email": "...", "password": "..." }
+        
         return "Login successful";
     }
 }
