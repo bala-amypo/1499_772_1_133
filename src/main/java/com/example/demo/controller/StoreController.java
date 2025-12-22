9e9be8ff051265b812be2837b58c7043530ef7ca
bala-amypo <bala@amypo.in>
2025-12-22 07:26:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/StoreController.java
+++ src/main/java/com/example/demo/controller/StoreController.java
@@ -1,18 +1,28 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Store;
+import com.example.demo.service.StoreService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/stores")
 public class StoreController {
 
+    private final StoreService storeService;
+
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
     @GetMapping
-    public String getAllStores() {
-        return "Stores fetched";
+    public List<Store> getAllStores() {
+        return storeService.getAllStores();
     }
 
     @PostMapping
-    public String createStore() {
-        return "Store created";
+    public Store createStore(@RequestBody Store store) {
+        return storeService.createStore(store);
     }
 }
