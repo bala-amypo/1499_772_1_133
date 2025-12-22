3b7ff2a834d095617b6913df18a207b86700148a
bala-amypo <bala@amypo.in>
2025-12-22 07:32:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/InventoryLevelController.java
+++ src/main/java/com/example/demo/controller/InventoryLevelController.java
@@ -1,28 +1,22 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.InventoryLevel;
-import com.example.demo.service.InventoryLevelService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/inventory")
 public class InventoryLevelController {
 
-    private final InventoryLevelService inventoryService;
-
-    public InventoryLevelController(InventoryLevelService inventoryService) {
-        this.inventoryService = inventoryService;
-    }
-
     @GetMapping
     public List<InventoryLevel> getAllInventory() {
-        return inventoryService.getAllInventory();
+        return new ArrayList<>(); // dummy response
     }
 
     @PostMapping
     public InventoryLevel addInventory(@RequestBody InventoryLevel inventoryLevel) {
-        return inventoryService.addInventory(inventoryLevel);
+        return inventoryLevel; // echo back request
     }
 }
