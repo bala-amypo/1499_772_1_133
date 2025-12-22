0743ddecbd4cc656b268d2b98ec0ea22970199e7
bala-amypo <bala@amypo.in>
2025-12-22 07:38:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,28 +1,22 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Product;
-import com.example.demo.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    private final ProductService productService;
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
-
     @GetMapping
     public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+        return new ArrayList<>(); // dummy list
     }
 
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
-        return productService.createProduct(product);
+        return product; // echo back
     }
 }
