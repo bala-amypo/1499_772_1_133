6a43dea897f68a92fe716f1318c45b3921c0f73c
bala-amypo <bala@amypo.in>
2025-12-26 10:19:32 +0000

    User commit

--- src/main/java/com/example/demo/service/InventoryService.java
+++ src/main/java/com/example/demo/service/InventoryService.java
@@ -3,7 +3,10 @@ package com.example.demo.service;
 import com.example.demo.entity.InventoryLevel;
 import com.example.demo.entity.Product;
 import com.example.demo.entity.Store;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.InventoryLevelRepository;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.repository.StoreRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,20 +15,26 @@ import java.util.List;
 public class InventoryService {
 
     private final InventoryLevelRepository inventoryRepo;
+    private final ProductRepository productRepo;
+    private final StoreRepository storeRepo;
 
-    public InventoryService(InventoryLevelRepository inventoryRepo) {
+    public InventoryService(InventoryLevelRepository inventoryRepo,
+                            ProductRepository productRepo,
+                            StoreRepository storeRepo) {
         this.inventoryRepo = inventoryRepo;
+        this.productRepo = productRepo;
+        this.storeRepo = storeRepo;
     }
 
     public InventoryLevel createInventory(Long productId, Long storeId, int quantity) {
+
+        Product product = productRepo.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+
+        Store store = storeRepo.findById(storeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Store not found"));
+
         InventoryLevel inventory = new InventoryLevel();
-
-        Product product = new Product();
-        product.setId(productId);
-
-        Store store = new Store();
-        store.setId(storeId);
-
         inventory.setProduct(product);
         inventory.setStore(store);
         inventory.setQuantity(quantity);
